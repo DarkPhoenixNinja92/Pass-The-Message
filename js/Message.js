5144f761af6ea9e3e9c3bd22746f0094ab05b4b1
Shaun Pour <scpour@gmail.com>
2021-06-10 12:06:26 -0400

    V3

--- js/Message.js
+++ js/Message.js
@@ -1,4 +1,8 @@
-function messageSubmit() {
+let messageSubmit = () => {
     const lastMessage = document.getElementById("messageIn").value;
     document.getElementById("myLastMessage").innerHTML = lastMessage;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("deliver-button").addEventListener('click', messageSubmit);
+})
